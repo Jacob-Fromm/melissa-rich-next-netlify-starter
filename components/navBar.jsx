c77029ccb59971692060a529c84b4ada01f6b042
Jacob Fromm <jacobfromm@gmail.com>
2023-03-01 21:59:56 -0500

    changed NavItem.jsx to navItem.jsx

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
-import NavItem from "./NavItem";
+import NavItem from "./navItem";
 
 const MENU_LIST = [
   { text: "HOME", href: "/" },
